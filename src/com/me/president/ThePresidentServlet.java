5b3225e7f08bfa248a22f9ce1fed3dc705a65ed2
Nacho Coloma <icoloma@gmail.com>
2014-02-20 11:49:38 +0100

    Added security

--- src/com/me/president/ThePresidentServlet.java
+++ src/com/me/president/ThePresidentServlet.java
@@ -3,10 +3,7 @@ package com.me.president;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.StringReader;
 import java.io.Writer;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServlet;
@@ -16,7 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
-import com.google.common.collect.ImmutableMap;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.common.collect.Maps;
 
 @SuppressWarnings("serial")
@@ -26,15 +25,24 @@ public class ThePresidentServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-	    InputStream template = getServletContext().getResourceAsStream("/WEB-INF/comments.html");
-		Mustache mustache = mf.compile(new InputStreamReader(template, "UTF-8"), "example");
-		
-		Map<String, Object> args = Maps.newHashMap();
-		args.put("name", "Obama?");
+	    InputStream template = getServletContext()
+	    		.getResourceAsStream("/WEB-INF/comments.html");
+		Mustache mustache = mf.compile(
+				new InputStreamReader(template, "UTF-8"), "example");
 		
 		Writer writer = resp.getWriter();
-	    mustache.execute(writer, args);
+	    mustache.execute(writer, initArgs(req));
 	    writer.flush();
+	}
+	
+	private Map<String, Object> initArgs(HttpServletRequest req) {
+		UserService userService = UserServiceFactory.getUserService();
+		Map<String, Object> args = Maps.newHashMap();
+		args.put("name", "Obama?");
+		args.put("user", userService.getCurrentUser());
+		args.put("loginUrl", userService.createLoginURL(req.getRequestURI()));
+		args.put("logoutUrl", userService.createLogoutURL(req.getRequestURI()));
+		return args;
 	}
 	
 }
